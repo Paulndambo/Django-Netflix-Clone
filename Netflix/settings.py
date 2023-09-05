92d95bda2660b57b9009e806c540503565949fdc
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-09-05 19:18:05 +0300

    local changes

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -120,13 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static_root'
-MEDIA_ROOT=BASE_DIR/'media'
-MEDIA_URL='/media/'
-
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
--- Netflix/urls.py
+++ Netflix/urls.py
@@ -10,9 +10,4 @@ urlpatterns = [
     path("", include("data.urls", namespace="data")),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-
-
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
